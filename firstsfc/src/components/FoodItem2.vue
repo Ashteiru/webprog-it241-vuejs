94446a8be0479b40c33ece6d89349c6b78a1afb5
Ashteiru <asgarcia@student.apc.edu.ph>
2026-01-17 08:17:34 +0000

    add fooditem 2

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'black apples',
+      message: 'I like black apples'
+    }
+  }
+};
+</script>
+
+<style></style>
