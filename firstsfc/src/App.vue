aa3981eafbbc598f0b559ce0e817a248d51e8762
Ashteiru <asgarcia@student.apc.edu.ph>
2026-01-29 12:31:41 +0800

    Add view and insert comment/visitor feedback functionality to your website

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,17 +1,13 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-const instruments = ref([])
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
-onMounted(() => {
-   getInstruments()
-})
-</script>
-<template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
 </template>
+
+  <country/>
+  <food-item/>
+  <personal-profile/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
+</template>
+
+<script></script>
